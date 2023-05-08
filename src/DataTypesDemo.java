f5de3530d0b114245d8b5395119b1452c82d0998
amolu <amolujagare@gmail.com>
2023-05-08 09:05:26 +0530

    Initial commit

---
+++ src/DataTypesDemo.java
@@ -0,0 +1,26 @@
+public class DataTypesDemo {
+
+    public static void main(String[] args) {
+
+        byte b = 4; // -128 to + 127 ( 8 bit )
+        short s = -32768; // -32768 to +32767 ( 16 bit )
+        int x = 7878990; //
+
+        float f = 23.56f;
+        double d = 23.56;
+
+        boolean resul1 = true;
+        boolean result2 = false;
+
+        char c1 = 'a';
+        char c2 = 'G';
+        char c3 = '!';
+        char c4 = '7';
+
+         char ch = 97 ; // no error
+        System.out.println("ch="+ch);
+         int x2 = 's' ;
+        System.out.println("x2="+x2);
+
+    }
+}
