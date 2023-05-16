b30bb58e1f8b96b1a5aaacda999afb87a76cd715
amolu <amolujagare@gmail.com>
2023-05-16 14:32:36 +0530

    java

---
+++ src/Arrays/InArrayDemo.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class InArrayDemo {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[6];
+
+        a[0] = 13;
+        a[1] = 56;
+        a[2] = 45;
+        a[3] = 11;
+        a[4] = 34;
+        a[5] = 78;
+
+        System.out.println("length="+a.length);
+
+        for (int i=0;i<a.length;i++)
+         System.out.println(a[i]);
+    }
+}
--- src/loops/FizBuzzProblem.java
+++ src/loops/FizBuzzProblem.java
@@ -4,7 +4,7 @@ public class FizBuzzProblem {
 
     public static void main(String[] args) {
         for(int i=1; i<=30 ; i++) {
-//15 , 30
+        //15 , 30
             if(i%3 == 0 && i%5 ==0)
                 System.out.println(i+" FizzBuzz");
             else if(i% 3 == 0)
